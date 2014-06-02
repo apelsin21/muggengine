cc4e7d54ab58cc855fa1e47688636356faccb9a2
Aksel Festin <drmugg123@gmail.com>
2014-06-02 18:43:41 +0200

    nothing really

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 title = "MuggEngine Lua window"
 
 window = Window.new(800, 600, title)
-window:set_vsync(true)
 
 input = InputHandler.new()
 
@@ -12,11 +11,11 @@ while window:is_open() == true do
     if input:is_key_down("Escape") == true then
         window:close()
     end
-
+    
     current_time = os.clock()
     frames = frames + 1
 
-    if(current_time - last_time) >= 1 then
+    if(current_time - last_time) >= 0.01 then
         print(1000/frames .. " ms/frame")
         frames = 0
         last_time = current_time
--- src/main.cpp
+++ src/main.cpp
@@ -11,76 +11,12 @@
 #include "windowbinds.hpp"
 #include "inputhandlerbinds.hpp"
 
-class Person {
-    private:
-        int age;
-    public:
-        Person(int age) {
-            this->age = age;
-        }
-        ~Person() {
-        }
-
-        void SetAge(int age) {
-            this->age = age;
-        }
-        int GetAge() {
-            return this->age;
-        }
-};
-
-Person* checkPerson(lua_State* L, int n) {
-    return *(Person**)luaL_checkudata(L, n, "lua_Person");
-}
-
-int personConstructor(lua_State* L) {
-    int arg = luaL_checknumber(L, 1);
-
-    Person** person = (Person**)lua_newuserdata(L, sizeof(Person*));
-    *person = new Person(arg);
-
-    luaL_getmetatable(L, "lua_Person");
-    lua_setmetatable(L, -2);
-
-    return 1;
-}
-
-int personSetAge(lua_State* L) {
-    Person* person = checkPerson(L, 1);
-    int arg = luaL_checknumber(L, 2);
-    person->SetAge(arg);
-
-    return 0;
-}
-
-int personGetAge(lua_State* L) {
-    Person* person = checkPerson(L, 1);
-    lua_pushnumber(L, person->GetAge());
-    return 1;
-}
-
-int personDeconstructor(lua_State* L) {
-    Person* person = checkPerson(L, 1);
-    delete person;
-    
-    return 0;
-}
-
-luaL_Reg personFuncs[] = {
-    {"new", personConstructor},
-    {"set_age", personSetAge},
-    {"get_age", personGetAge},
-    {"__gc", personDeconstructor},
-    {NULL, NULL}
-};
-
 int main() {
     mugg::ScriptSystem system(true);
 
     system.RegisterMetatable(mugg::binds::windowFuncs, "mugg_Window", "Window");
     system.RegisterMetatable(mugg::binds::inputHandlerFuncs, "mugg_InputHandler", "InputHandler");
-
-	system.DoFile("main.lua");
-
+ 
+    system.DoFile("main.lua");
     return 0;
 }
