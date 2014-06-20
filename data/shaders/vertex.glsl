8d4acc6033dd0754f6803718d1119c918b9c7ee0
Aksel Festin <drmugg123@gmail.com>
2014-06-20 23:50:08 +0200

    added rendering a triangle for testing purposes

--- data/shaders/vertex.glsl
+++ data/shaders/vertex.glsl
@@ -1,5 +1,7 @@
 #version 130
 
+in vec4 position;
+
 void main() {
-    gl_Position = vec4(0, 0, 0, 0); 
+    gl_Position = position; 
 }
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -52,6 +52,39 @@ void mugg::graphics::Renderer::BeginRender(glm::vec2 viewportResolution = glm::v
     if(this->programVector.size() != 0) {
         for(int i = 0; i < this->programVector.size(); i++) {
             glUseProgram(this->programVector[i].GetID());
+
+            GLuint vaoID;
+            glGenVertexArrays(1, &vaoID);
+            glBindVertexArray(vaoID);
+
+            static const GLfloat vertices[] = {
+               -1.0f, -1.0f, 0.0f, 1.0f,
+               1.0f, -1.0f, 0.0f, 1.0f,
+               0.0f,  1.0f, 0.0f, 1.0f,
+            };
+
+            GLuint vboID;
+            glGenBuffers(1, &vboID);
+            glBindBuffer(GL_ARRAY_BUFFER, vboID);
+            
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+            // 1rst attribute buffer : vertices
+            glEnableVertexAttribArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, vboID);
+            glVertexAttribPointer(
+               0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+               4,                  // size
+               GL_FLOAT,           // type
+               GL_FALSE,           // normalized?
+               0,                  // stride
+               (void*)0            // array buffer offset
+            );
+             
+            // Draw the triangle !
+            glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+             
+            glDisableVertexAttribArray(0);
         }
     }
 }
